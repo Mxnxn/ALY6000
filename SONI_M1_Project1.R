fd1ba0c187b5fb9813ef057fb1d5b1878fbdafb5
unknown <manan@protonmail.com>
2022-01-20 11:02:49 +0530

    - Final v1

--- SONI_M1_Project1.R
+++ SONI_M1_Project1.R
@@ -1,66 +1,43 @@
-# check if package exist
+# check if package exist and installing package ‘vcd’ 
 if(!require('vcd',character.only = TRUE)) install.packages('vcd')
-# importing vcd
+# importing ‘vcd’
 library(vcd)
 
 # creating Vectors
 sales <- c(7,11,15,20,19,11,18,10,6,22)
 temperature = c(69,81,77,84,80,97,87,70,65,90)
-sales
-temperature 
 
 # plotting scatter graph for sales ~ temperature
 library(ggplot2)
-ggplot(data.frame(sales,temperature), aes(x = sales, y = temperature, color = ..y..)) +
+ggplot(data.frame(sales,temperature), aes(x = temperature , y=sales , color = ..y..)) +
   geom_point(size=4) +
   labs(
-    title = "Fig.1 Sales ~ Tempareture", # adds title
-    x = "Sales", # x-axis label
-    y = "Temperature (F)", # y-axis label
-    color = "Temperature" # color legend
-  ) + 
-  scale_color_gradient(low="blue", high="orange") +
-  theme_light() +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-  )
-plot(sales,temperature)
+    title = "Fig.1 Sales ~ Temperature", # adds title
+    x = "Temperature (°F)", # x-axis label
+    y = "Sales\n", # y-axis label
+    color = "Sales" # color legend
+  ) + scale_color_gradient(low="pink", high="red") + theme_light() +
+  theme(plot.title = element_text(hjust = 0.5))
 
-# getting mean of temperature
-mean_temperature <- mean(temperature)
-mean_temperature
+meanOfTemperature <- mean(temperature) # Mean of temperature Vector
 
-# Removing From Index 15
+# Removing From Index which is 3 as Indexing starts from 1 in ‘R’
 sales <- c(sales[-3])
-sales
-# Insert 16 at 2 index
+# Insert 16 at index 2
 sales <- append(sales,16,2)
-sales 
 
-# creating Vectors of String/Names
+# allocationg Vector of strings in <names> Vector
 names <- c("Tom","Dick","Harry")
-names
 
+mMatrix <- matrix(c(1:10),nrow=5,byrow=FALSE) # creating 5x2 Matrix
 
-matrixx <- matrix(c(1:10),nrow=5,byrow=FALSE) # creating Matrix of 2 column and 5 row
-matrixx
-
-# creating dataFrame from sales, temperature
+# creating dataFrame from sales, temperature named <icSales>
 icSales <- data.frame('Sales'= sales,'Temperature' = temperature)
-icSales
-
 print(str(icSales))
 
-# finding summary of Data Frame
-summaryx = summary(icSales)
-summaryx
-
-
-student_data = read.csv("./Student.csv") # Opening Student.csv file
+# finding summary of Data Frame <icSales>
+mSummary = summary(icSales)
+# to open Student.csv file read.csv is built in function provided by ‘R’
+student_data = read.csv("./Student.csv") # change CWD for this command
+# two functions names() and col_names() which are built-in in ‘R’
 names(student_data) # Printing Variables of Student.csv
-
-
-# ref
-# https://www.tutorialspoint.com/r/index.htm
-# Book: R in Action Data Analysis and graphics with R by Robert I. Kabacoff
-
